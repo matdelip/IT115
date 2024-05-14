ad3717d17349703dcc3cca61e3e7f8e530040264
matdelip <matdeli.parra@seattlecolleges.edu>
2024-05-14 12:41:55 -0700

    First Push

---
+++ json.py
@@ -0,0 +1,11 @@
+import json
+
+# create the data '' dictionary
+date = {
+
+    'name': 'John Doe',
+    'age': 30,
+    'city': 'New York, NY'
+    'interests': ['Golf', 'Running', 'Football', 'Traveling']
+    'is student': False
+}
